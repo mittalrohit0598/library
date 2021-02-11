744f08d9929e3df8544a0a42064ab49e4a9a2f63
Rohit Mittal <mittalrohit0598@gmail.com>
2021-02-11 21:25:09 +0530

    Add script to display books with dummy values

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,47 @@ function Book(title, author, NoOfPages, readYet) {
 }
 
 let hobbit = new Book("abc", "def", 123, false);
-console.log(hobbit.info());
+let hobbit2 = new Book("xyz", "ghi", 123, false);
+let hobbit3 = new Book("qrst", "lmn", 123, false);
+
+let myLibrary = [];
+
+myLibrary.push(hobbit);
+myLibrary.push(hobbit2);
+myLibrary.push(hobbit3);
+
+console.log(myLibrary)
 
 function toggleForm() {
     let div = document.getElementById("add-book-form");
     div.style.display = div.style.display == "none" || div.style.display.length == 0 ? "block" : "none";
+}
+
+Object.prototype.deleteBook = function() {
+    this.parentNode.parentNode.removeChild(this.parentNode);
+}
+
+window.onload = function() {
+    myLibrary.forEach(book => {
+        let cardDiv = document.createElement("div");
+        cardDiv.className = "card";
+        let titleDiv = document.createElement("div");
+        titleDiv.className = "title";
+        titleDiv.innerHTML = book.title;
+        let authorDiv = document.createElement("div");
+        authorDiv.className = "author";
+        authorDiv.innerHTML = book.author;
+        let deleteButton = document.createElement("input");
+        deleteButton.value = "Delete";
+        deleteButton.type = "button"
+        deleteButton.addEventListener('click', deleteBook, false);
+        let changeStatusButton = document.createElement("input");
+        changeStatusButton.value = "ChangeReadStatus";
+        changeStatusButton.type = "button"
+        cardDiv.appendChild(titleDiv);
+        cardDiv.appendChild(authorDiv);
+        cardDiv.appendChild(deleteButton);
+        cardDiv.appendChild(changeStatusButton);
+        document.getElementById("books").appendChild(cardDiv);
+    });
 }
